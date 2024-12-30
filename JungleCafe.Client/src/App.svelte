87008da7f217cf28b295e026dfe130b0b0db288d
Kacper Szafrański <kacperr.szafranski@gmail.com>
2024-12-30 22:09:21 +0100

    Move files out of JungleCafe dir

---
+++ JungleCafe.Client/src/App.svelte
@@ -0,0 +1,6 @@
+<!-- App.svelte -->
+<script>
+  import Greeting from './lib/Greeting.svelte';
+</script>
+
+<Greeting name="Jan" color="red" />
---
+++ JungleCafe.Client/src/lib/Greeting.svelte
@@ -0,0 +1,9 @@
+<!-- lib/Greeting.svelte -->
+<script>
+    export let name = 'Świat';  // export oznacza prop, można ustawić wartość domyślną
+    export let color = 'black';
+</script>
+
+<h1 style="color: {color}">
+    Witaj, {name}!
+</h1>
