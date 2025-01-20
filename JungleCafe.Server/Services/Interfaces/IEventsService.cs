5cad8f89939a6c9f186d819173f3db644fcc3a72
Kacper Szafrański <kacperr.szafranski@gmail.com>
2025-01-20 02:55:06 +0100

    IEventsService.cs refactors

--- JungleCafe.Server/Services/Interfaces/IEventsService.cs
+++ JungleCafe.Server/Services/Interfaces/IEventsService.cs
@@ -1,10 +1,11 @@
-﻿using JungleCafe.Server.RequestModels;
+﻿using JungleCafe.Server.Models;
+using JungleCafe.Server.RequestModels;
 
 namespace JungleCafe.Server.Services.Interfaces;
 
 public interface IEventService
 {
-    Task<object> GetEvent(int id);
-    Task<IEnumerable<object>> GetEvents();
+    Task<Event?> GetEvent(int id);
+    Task<IEnumerable<Event>> GetEvents();
     Task RegisterForEvent(EventRegistrationRequest request, int userId);
 }
